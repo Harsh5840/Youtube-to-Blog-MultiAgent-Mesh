9ad209b68aa967f49ccf1b6f9e040489de66dd3d
Harsh5840 <shuklaharsh5840@gmail.com>
2025-07-20 23:23:30 +0530

    agent initailizing

--- agents.py
+++ agents.py
@@ -3,3 +3,8 @@ from crewai import Agent
 
 
 #senior blog-agent
+
+blog_researcher=Agent(
+    role='Blog Researcher from Youtube Videos',
+    goal='get the relevant video content for the topic{topic} from youtube'
+)
